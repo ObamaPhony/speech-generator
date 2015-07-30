de6cfd735afcf0147952d13c398bdb422d182df4
Saleem Rashid <spudowiar@outlook.com>
2015-07-30 16:44:50 +0100

    initial commit

---
+++ speech-generator.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# usage: $0 TOPIC [TOPIC...] NUMBER
+#
+import sys, json, random, re
+
+# amount of nouns replaced
+REPLACEMENT = 0.5
+# chance of choosing an unrelated topic word
+UNPREDICTABILITY = 0.2
+
+def speechiterator(analysis):
+    for speech in analysis:
+        for paragraph in speech[1:-1]:
+            yield paragraph
+
+# replace the nth occurence of needle in haystack
+def replace(haystack, needle, replacement, n):
+    escaped = re.escape(needle)
+    if n > 1:
+        regex = r'^((.*?\b%s\b.*?){%d})\b%s\b' % (escaped, n - 1, escaped)
+    else:
+        regex = r'^(.*?)\b%s\b' % escaped
+    return re.sub(regex, r'\1%s' % replacement, haystack)
+
+# read analysis from stdin 
+analysis = json.load(sys.stdin)
+
+# command-line arguments
+topics = sys.argv[1:-1]
+n = int(sys.argv[-1])
+
+# dictionary of 'topic: [paragraph, ]'
+paragraphs = dict(zip(topics, random.sample(list(speechiterator(analysis)), n * len(topics))[::n]))
+
+for topic, paragraph in paragraphs.items():
+    for sentence in paragraph:
+        others = topics[:]
+        if len(others) > 1:
+            others.remove(topic)
+        nouns = [word[0] for word in sentence["summary"] if word[1][:2] == "NN"]
+        indexes = random.sample(range(len(nouns)), int(len(nouns) * REPLACEMENT))
+        for i in indexes:
+            # which occurence of nouns[i] is this?
+            occurence = nouns[:i].count(nouns[i]) + 1
+            # which topic to use
+            replacement = random.choice(others) if random.random() < UNPREDICTABILITY else topic
+            sentence["sentence"] = replace(sentence["sentence"], nouns[i], replacement, occurence)
+        print(sentence["sentence"], end=". ")
+    print()
